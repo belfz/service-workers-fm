a19ef72f57eaf940b85bbe05c436dacb5ea2a5e2
belfz <marcinbar1@gmail.com>
2018-01-25 14:50:39 +0100

    messing with document DOMContentLoaded and window.onload

--- demo/public/app.js
+++ demo/public/app.js
@@ -1,32 +1,34 @@
-const sendButton = document.getElementById('send');
-const result = document.getElementById('result');
-const reloadNotification = document.getElementById('reload-notification');
+document.addEventListener('DOMContentLoaded', () => {
+  const sendButton = document.getElementById('send');
+  const result = document.getElementById('result');
+  const reloadNotification = document.getElementById('reload-notification');
 
-sendButton.addEventListener('click', () => {
-  fetch('/data', {
-    method: 'post',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ message: 'ping!' })
-  })
-  .then(({ status, statusText }) => {
-    switch (status) {
-      case 202:
-        result.innerText = `No connection. Request buffered for retrial when app is online again.`;
-        result.className = 'result-warning';
-        break;
-      case 200:
-      default:
-        result.innerText = `${status} ${statusText}`;
-        result.className = 'result-ok';
-        break;
-    }
-  })
-  .catch(() => {
-    result.innerText = `Other error occured. Check if the server is running.`;
-    result.className = 'result-warning';
+  sendButton.addEventListener('click', () => {
+    fetch('/data', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: 'ping!' })
+    })
+    .then(({ status, statusText }) => {
+      switch (status) {
+        case 202:
+          result.innerText = `No connection. Request buffered for retrial when app is online again.`;
+          result.className = 'result-warning';
+          break;
+        case 200:
+        default:
+          result.innerText = `${status} ${statusText}`;
+          result.className = 'result-ok';
+          break;
+      }
+    })
+    .catch(() => {
+      result.innerText = `Other error occured. Check if the server is running.`;
+      result.className = 'result-warning';
+    });
   });
 });
 
